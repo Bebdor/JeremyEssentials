7a5725db2bf355851356d612d46551da21f6c965
Bebdor <oemer@kersikoglu.de>
2024-01-15 17:18:07 +0100

    Fixed CommandSpyListener

--- src/main/java/de/devlucas/jeremyessentials/commands/admin/commandspy/CommandSpyListener.java
+++ src/main/java/de/devlucas/jeremyessentials/commands/admin/commandspy/CommandSpyListener.java
@@ -1,25 +1,21 @@
 package de.devlucas.jeremyessentials.commands.admin.commandspy;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
+import de.devlucas.jeremyessentials.JeremyEssentials;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class CommandSpyListener implements Listener {
-    private CommandSpyCommand commandSpyCommand = new CommandSpyCommand();
 
     @EventHandler
     public void onCommandPreProcess(@NotNull PlayerCommandPreprocessEvent playerCommandPreprocessEvent) {
-        String playerName = playerCommandPreprocessEvent.getPlayer().getName();
 
-        if (commandSpyCommand.cmdSpyMap.containsKey(playerName)) {
-            String command = playerCommandPreprocessEvent.getMessage().split(" ")[0];
-
-            if(commandSpyCommand.shouldSpy(playerName, command)){
-                Bukkit.broadcastMessage(ChatColor.GRAY + playerName + " used the command: " + ChatColor.WHITE + playerCommandPreprocessEvent.getMessage());
-            }
+        for (final Player player : JeremyEssentials.commandspyList)
+        {
+            player.sendMessage(JeremyEssentials.pre + playerCommandPreprocessEvent.getPlayer().getName() + " used command: §6" + playerCommandPreprocessEvent.getMessage());
         }
+
     }
 }
